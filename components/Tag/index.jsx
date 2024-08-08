b10adf37b94796d2178ec3b6f19b2003753cfaf7
Ferdi Yankovic <yankovicferdi@gmail.com>
2024-08-09 01:55:54 +0530

    feat: resolve comments

--- components/Tag/index.jsx
+++ components/Tag/index.jsx
@@ -6,14 +6,10 @@ const Tag = ({ title, handleTagClick, checked }) => {
 		<div
 			role='none'
 			onClick={() => handleTagClick(title)}
-			className={`flex size-fit cursor-pointer items-center space-x-2 rounded-full border-[0.061rem] border-[#010001] transition-all duration-100 ease-in md:border ${
+			className={`flex font-gm text-center text-[0.875rem] leading-5 tracking-[-0.02em] font-[600] size-fit cursor-pointer items-center space-x-2 rounded-full border-[0.061rem] border-[#010001] transition-all duration-100 ease-in md:border ${
 				checked ? "bg-[#010001] text-white" : "bg-transparent text-[#010001]  hover:bg-[rgba(0,0,0,0.2)]"
-				} py-[0.619rem] px-[0.967rem]`}>
-			<div
-				// eslint-disable-next-line tailwindcss/no-custom-classname
-				className='font-gm text-center text-[1rem] leading-5 tracking-[-0.02em] font-[600] text-[#010001]'>
-				{title}
-			</div>
+				} py-[0.4rem] px-[1.4rem]`}>
+			{title}
 		</div>
 	);
 };
